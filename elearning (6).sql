30b62ff406dad28ea25c5ce9f1d5cae5bbbfe424
MUKESHIMANA928 <150811329+MUKESHIMANA928@users.noreply.github.com>
2025-06-11 14:15:19 +0200

    Add files via upload

---
+++ elearning (6).sql
@@ -0,0 +1,462 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2025 at 02:08 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `elearning`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollments`
+--
+
+CREATE TABLE `enrollments` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `enrolled_at` datetime DEFAULT current_timestamp(),
+  `completed` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `enrollments`
+--
+
+INSERT INTO `enrollments` (`id`, `user_id`, `module_id`, `enrolled_at`, `completed`) VALUES
+(1, 14, 1, '2025-06-07 22:50:50', 0),
+(2, 5, 1, '2025-06-07 23:46:46', 1),
+(3, 17, 1, '2025-06-10 00:35:31', 0),
+(4, 17, 6, '2025-06-10 09:43:09', 0),
+(5, 17, 7, '2025-06-10 09:43:27', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modules`
+--
+
+CREATE TABLE `modules` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `published` tinyint(1) DEFAULT 0,
+  `lecturer_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `modules`
+--
+
+INSERT INTO `modules` (`id`, `title`, `description`, `published`, `lecturer_id`) VALUES
+(1, 'Networking', 'Making RJ-45 cables and other networking cables', 0, 23),
+(6, 'Programing with clients server', 'Eugene is the creator of this module. ', 0, 23),
+(7, 'Networking and its contents', 'made by the administrator ', 0, 23),
+(10, 'RMT ', '15creadit', 1, 23);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `module_contents`
+--
+
+CREATE TABLE `module_contents` (
+  `id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `content_type` varchar(50) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `original_filename` varchar(255) NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `display_order` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `module_contents`
+--
+
+INSERT INTO `module_contents` (`id`, `module_id`, `content_type`, `title`, `description`, `file_path`, `original_filename`, `uploaded_at`, `display_order`) VALUES
+(1, 1, 'pdf', 'the basics of networking cables', 'Let\'s know how to create these cables', 'uploads\\contentFile-1748955712112.pdf', 'ETHERNET CABLE.pdf', '2025-06-03 13:01:52', 1),
+(7, 1, 'video', 'Wireless technology', 'wireless handover', '/uploads/contentFile-1749396037376.mp4', 'this.mp4', '2025-06-08 15:20:37', 0),
+(8, 1, 'presentation', 'networking notes', 'these notes will help the learners to increase their knowledge of wireless networking', '/uploads/contentFile-1749400231201.pptx', '9march.pptx', '2025-06-08 16:30:31', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
+
+CREATE TABLE `options` (
+  `id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `option_text` varchar(255) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `paid_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `question_text` text NOT NULL,
+  `question_type` enum('multiple_choice','true_false','short_answer') DEFAULT 'multiple_choice',
+  `display_order` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id`, `quiz_id`, `question_text`, `question_type`, `display_order`) VALUES
+(2, 2, 'Briefly, explain the importance of networking.', 'short_answer', 0),
+(3, 2, 'What are the types of network cable?', 'short_answer', 1),
+(4, 3, 'what is progrmming?', 'short_answer', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizzes`
+--
+
+CREATE TABLE `quizzes` (
+  `id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `duration_minutes` int(11) DEFAULT NULL,
+  `pass_percentage` int(11) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `quizzes`
+--
+
+INSERT INTO `quizzes` (`id`, `module_id`, `title`, `description`, `duration_minutes`, `pass_percentage`, `created_at`, `updated_at`) VALUES
+(2, 7, 'bit 3 first quize', 'do all questions', 26, 58, '2025-06-10 07:22:53', '2025-06-10 07:22:53'),
+(3, 6, 'bit 3', '10 credits', 6, 35, '2025-06-11 10:18:43', '2025-06-11 10:18:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_answers`
+--
+
+CREATE TABLE `quiz_answers` (
+  `id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `submission_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `submitted_text` text DEFAULT NULL,
+  `selected_option_id` int(11) DEFAULT NULL,
+  `is_correct` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `quiz_answers`
+--
+
+INSERT INTO `quiz_answers` (`id`, `question_id`, `submission_id`, `user_id`, `submitted_text`, `selected_option_id`, `is_correct`) VALUES
+(2, 2, 2, 17, NULL, NULL, 0),
+(3, 3, 2, 17, NULL, NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_submissions`
+--
+
+CREATE TABLE `quiz_submissions` (
+  `id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `score` decimal(5,2) NOT NULL,
+  `passed` tinyint(1) NOT NULL,
+  `submitted_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `quiz_submissions`
+--
+
+INSERT INTO `quiz_submissions` (`id`, `quiz_id`, `user_id`, `score`, `passed`, `submitted_at`) VALUES
+(2, 2, 17, 0.00, 0, '2025-06-10 07:48:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('learner','lecturer','administrator') DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `role`, `active`) VALUES
+(5, 'HABIMANA', 'Charles', '@charles', 'habimana@gmail.com', '123456', 'lecturer', 0),
+(14, 'HIRWA', 'Aime', 'Aimerance', 'hirwaaimerance@gmail.com', '$2b$10$s6NALK69z3LOKXkc.dTR3uSImDp8SGNh1QzoumRd5EyfFesRP3Xc6', 'learner', 1),
+(17, 'UWABERA', 'Alice', '@alice', 'uwabera@gmail.com', '$2b$10$HoEq72E22NbrkVJz1VQYLe7yXAFBzcR/IugMZSNMGzq.BjtVdHMGG', 'learner', 1),
+(23, 'uwineza', 'claire', 'uclaire', 'uclaire@gmail.com', '$2b$10$dS2VNb3CtRz3cY1FCxvKpu1hKxwC0t4NlLlQU/k2M9yj4BFWh4G.a', 'lecturer', 1),
+(24, 'MUKESHIMANA', 'Eugene', '@eugene', 'eugene@gmail.com', '$2b$10$0KsRi2HZjmIPa6upf7cSMuhAh0hYBLC8YlYeEkkGb1gqBuefwOcq2', 'administrator', 1),
+(25, 'emmy', 'izere', 'emmy@', 'izere@gmail.com', '$2b$10$Nz0t71IUvDXV.U1ncZjPT.8SGGHRY/nMqcYWR0D8RcocqeijHAzCu', 'lecturer', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `module_id` (`module_id`);
+
+--
+-- Indexes for table `modules`
+--
+ALTER TABLE `modules`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_lecturer_id` (`lecturer_id`);
+
+--
+-- Indexes for table `module_contents`
+--
+ALTER TABLE `module_contents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `module_id` (`module_id`);
+
+--
+-- Indexes for table `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `question_id` (`question_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `module_id` (`module_id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `quiz_id` (`quiz_id`);
+
+--
+-- Indexes for table `quizzes`
+--
+ALTER TABLE `quizzes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `module_id` (`module_id`);
+
+--
+-- Indexes for table `quiz_answers`
+--
+ALTER TABLE `quiz_answers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `question_id` (`question_id`),
+  ADD KEY `submission_id` (`submission_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `selected_option_id` (`selected_option_id`);
+
+--
+-- Indexes for table `quiz_submissions`
+--
+ALTER TABLE `quiz_submissions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `quiz_id` (`quiz_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `modules`
+--
+ALTER TABLE `modules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `module_contents`
+--
+ALTER TABLE `module_contents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `options`
+--
+ALTER TABLE `options`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `quizzes`
+--
+ALTER TABLE `quizzes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `quiz_answers`
+--
+ALTER TABLE `quiz_answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `quiz_submissions`
+--
+ALTER TABLE `quiz_submissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `registration` (`id`),
+  ADD CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`);
+
+--
+-- Constraints for table `modules`
+--
+ALTER TABLE `modules`
+  ADD CONSTRAINT `fk_lecturer_id` FOREIGN KEY (`lecturer_id`) REFERENCES `registration` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `module_contents`
+--
+ALTER TABLE `module_contents`
+  ADD CONSTRAINT `module_contents_ibfk_1` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `options`
+--
+ALTER TABLE `options`
+  ADD CONSTRAINT `options_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `registration` (`id`),
+  ADD CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`);
+
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `quizzes`
+--
+ALTER TABLE `quizzes`
+  ADD CONSTRAINT `quizzes_ibfk_1` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `quiz_answers`
+--
+ALTER TABLE `quiz_answers`
+  ADD CONSTRAINT `quiz_answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `quiz_answers_ibfk_2` FOREIGN KEY (`submission_id`) REFERENCES `quiz_submissions` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `quiz_answers_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `registration` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `quiz_answers_ibfk_4` FOREIGN KEY (`selected_option_id`) REFERENCES `options` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `quiz_submissions`
+--
+ALTER TABLE `quiz_submissions`
+  ADD CONSTRAINT `quiz_submissions_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `quiz_submissions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `registration` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
